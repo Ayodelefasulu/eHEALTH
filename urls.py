f50ee9121aa19781f968d0bcf774283dc7a791f9
ayodelefasulu <fasuluayodele@gmail.com>
2025-03-02 16:51:38 +0100

    appointment model, urls.py for app and project, defining endpoints 2

--- urls.py
+++ urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
+import rest_framework
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
